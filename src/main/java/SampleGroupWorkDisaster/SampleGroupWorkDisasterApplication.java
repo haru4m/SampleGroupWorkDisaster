63880e5ec1bcc13f7f2ce6341a835d0020fd8f33
DESKTOP-8GHT8U8\NPC-71 <haru4m.work@gmail.com>
2025-05-23 16:48:59 +0900

    aaa

---
+++ src/main/java/SampleGroupWorkDisaster/SampleGroupWorkDisasterApplication.java
@@ -0,0 +1,13 @@
+package SampleGroupWorkDisaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGroupWorkDisasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGroupWorkDisasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SampleGroupWorkDisaster/SampleGroupWorkDisasterApplicationTests.java
@@ -0,0 +1,13 @@
+package SampleGroupWorkDisaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGroupWorkDisasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
